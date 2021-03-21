6b07543d21ddb8bad1079a55c701e9a4ba0e0556
Juancarlosarbelaez <juan.arbelaezm@upb.edu.co>
2021-03-21 09:32:57 -0500

    Agregada la clase establecimiento

---
+++ UMLBased/Establecimiento.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using KYLib.MathFn;
+using DO = Newtonsoft.Json.JsonObjectAttribute;
+using DP = Newtonsoft.Json.JsonPropertyAttribute;
+
+namespace AppDomicilioSharp.UMLBased
+{
+	[DO(Newtonsoft.Json.MemberSerialization.OptIn)]
+	/// <summary>
+	/// Representa una cuenta de un establecimiento 
+	/// </summary>
+	public sealed class Establecimiento : Cuenta
+	{
+        private List<Producto> menu;
+        private List<Categoria> categorias;
+        private List<byte> calificacion;
+        private List<DateTime> atencion_al_cliente;
+        private List<Pedido> pedidos;
+
+        public List<Producto> Menu { get => menu; }
+        public List<Categoria> Categorias { get => categorias; }
+        public List<byte> Calificacion { get => calificacion; }
+        public List<DateTime> Atencion_al_cliente { get => atencion_al_cliente;  }
+        public List<Pedido> Pedidos { get => pedidos;  }
+
+
+        public bool AgregarPedido(Factura factura) 
+        {
+            return true;
+        }
+
+
+        public bool AgregarPedidos(List<Factura> facturas)
+        {
+            return true;
+        }
+
+        public void Calificar(byte calificacion)
+        {
+            this.calificacion = calificacion;
+            return true;
+        }
+    }
+}
