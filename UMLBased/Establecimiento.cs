6e136e59dd4172369b8430f5ba86936ae0bc0991
JuanCalle1606 <jpmilojp@gmail.com>
2021-03-23 11:16:07 -0500

    Agregado el calculo del promedio de la calificacion en el Establecimiento

--- UMLBased/Establecimiento.cs
+++ UMLBased/Establecimiento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using KYLib.Extensions;
 using KYLib.MathFn;
 using DO = Newtonsoft.Json.JsonObjectAttribute;
 using DP = Newtonsoft.Json.JsonPropertyAttribute;
@@ -12,35 +13,34 @@ namespace AppDomicilioSharp.UMLBased
 	/// </summary>
 	public sealed class Establecimiento : Cuenta
 	{
-        private List<Producto> menu;
-        private List<Categoria> categorias;
-        private List<byte> calificacion;
-        private List<DateTime> atencion_al_cliente;
-        private List<Pedido> pedidos;
+		private List<Producto> menu;
+		private List<Categoria> categorias;
+		private List<byte> calificacion;
+		private List<DateTime> atencion_al_cliente;
+		private List<Pedido> pedidos;
 
-        public List<Producto> Menu { get => menu; }
-        public List<Categoria> Categorias { get => categorias; }
-        public List<byte> Calificacion { get => calificacion; }
-        public List<DateTime> Atencion_al_cliente { get => atencion_al_cliente;  }
-        public List<Pedido> Pedidos { get => pedidos;  }
-
-
-        public bool AgregarPedido(Factura factura) 
-        {
-            return true;
-        }
+		public List<Producto> Menu { get => menu; }
+		public List<Categoria> Categorias { get => categorias; }
+		public byte Calificacion => (byte)Mathf.MeanOf(calificacion.ToArray<byte, Int>(s => s));
+		public List<DateTime> Atencion_al_cliente { get => atencion_al_cliente; }
+		public List<Pedido> Pedidos { get => pedidos; }
 
 
-        public bool AgregarPedidos(List<Factura> facturas)
-        {
-            return true;
-        }
+		public bool AgregarPedido(Factura factura)
+		{
+			return true;
+		}
 
-        public bool Calificar(byte calificacion)
-        {
-            byte score = calificacion;
-            this.calificacion.Add(score);
-            return true;
-        }
-    }
+
+		public bool AgregarPedidos(List<Factura> facturas)
+		{
+			return true;
+		}
+
+		public bool Calificar(byte puntuacion)
+		{
+			calificacion.Add(puntuacion);
+			return true;
+		}
+	}
 }
