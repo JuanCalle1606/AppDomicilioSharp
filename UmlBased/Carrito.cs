d51f319c026f891578422b629d06806f7dd193be
Juancarlosarbelaez <juan.arbelaezm@upb.edu.co>
2021-04-09 20:45:02 -0500

    Agregada la documentacion de la clase carrito y la implementacion de 2 metodos

--- UmlBased/Carrito.cs
+++ UmlBased/Carrito.cs
@@ -11,12 +11,23 @@ namespace UmlBased
     /// </summary>
     public sealed class Carrito
     {
+        /// <summary>
+        /// Representa el costo de los pedidos almacenados en el carrito mas el IVA.
+        /// </summary>
         private double total;
-        private List<Pedido> productos;
+
+        /// <summary>
+        /// Representa una lista de los pedidos del cliente.
+        /// </summary>
+        private List<Pedido> pedidos;
+
+        /// <summary>
+        /// Representa el costo de los pedidos almacenados en el carrito.
+        /// </summary>
         private double costo;
 
         public double Total { get => total; }
-        public List<Pedido> Productos { get => productos; }
+        public List<Pedido> Pedidos { get => pedidos; }
         public double Costo { get => costo; }
 
         public Carrito()
@@ -25,24 +36,32 @@ namespace UmlBased
 
         }
 
-        public bool Agregar(Producto producto, double pago)
+        /// <summary>
+        /// Agrega un pedido al carrito.
+        /// </summary>
+        /// <param name="pedido">Pedido que se desea aregar al carrito.</param>
+        /// <returns>Devuelve un booleano que indica si se pudo agregar el pedido.</returns>
+        public bool Agregar(Pedido pedido)
         {
+            Pedidos.Add(pedido);
             return true;
         }
 
-        public bool Agregar(Producto producto)
-        {
-            return true;
+
+
+        public bool Pagar() //Esta funcion debe ir en el comprador puesto que se necesita acceso 
+        {                   //a su saldo y de acuerdo con el diagrama UML la clase carrito no puede
+            return true;    //ver a la clase comprador
         }
 
-        public bool Pagar()
+        /// <summary>
+        /// Elimina un pedido del carrito.
+        /// </summary>
+        /// <param name="pedido">Pedido que se desea elimiar del carrito.</param>
+        /// <returns>Devuelve un booleano que indica si se pudo eliminar el pedido.</returns>
+        public bool Eliminar(Pedido pedido)
         {
-            return true;
-        }
-
-        public bool Eliminar(Producto producto)
-        {
-
+            Pedidos.Remove(pedido);
             return true;
         }
     }
