f8b26c2c1270be1ded76375ccb51be31c8c7e208
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-10 21:03:00 -0500

    Se cambio el nombre d euna propiedad en carrito, y la propiedad Carrito en Comprador

--- UmlBased/Carrito.cs
+++ UmlBased/Carrito.cs
@@ -5,64 +5,64 @@ using DP = Newtonsoft.Json.JsonPropertyAttribute;
 
 namespace UmlBased
 {
-    [DO(Newtonsoft.Json.MemberSerialization.OptIn)]
-    /// <summary>
-    /// Representa una cuenta de un establecimiento 
-    /// </summary>
-    public sealed class Carrito
-    {
-        /// <summary>
-        /// Representa el costo de los pedidos almacenados en el carrito mas el IVA.
-        /// </summary>
-        private double total;
+	[DO(Newtonsoft.Json.MemberSerialization.OptIn)]
+	/// <summary>
+	/// Representa una cuenta de un establecimiento 
+	/// </summary>
+	public sealed class Carrito
+	{
+		/// <summary>
+		/// Representa el costo de los pedidos almacenados en el carrito mas el IVA.
+		/// </summary>
+		private double total;
 
-        /// <summary>
-        /// Representa una lista de los pedidos del cliente.
-        /// </summary>
-        private List<Pedido> pedidos;
+		/// <summary>
+		/// Representa una lista de los pedidos del cliente.
+		/// </summary>
+		private List<Pedido> pedidos;
 
-        /// <summary>
-        /// Representa el costo de los pedidos almacenados en el carrito.
-        /// </summary>
-        private double costo;
+		/// <summary>
+		/// Representa el costo de los pedidos almacenados en el carrito.
+		/// </summary>
+		private double costo;
 
-        public double Total { get => total; }
-        public List<Pedido> Pedidos { get => pedidos; }
-        public double Costo { get => costo; }
+		public double Total { get => total; }
+		public List<Pedido> Pedidos { get => pedidos; }
+		public double CostoCuota { get => costo; }
 
-        public Carrito()
-        {
-
-
-        }
-
-        /// <summary>
-        /// Agrega un pedido al carrito.
-        /// </summary>
-        /// <param name="pedido">Pedido que se desea aregar al carrito.</param>
-        /// <returns>Devuelve un booleano que indica si se pudo agregar el pedido.</returns>
-        public bool Agregar(Pedido pedido)
-        {
-            Pedidos.Add(pedido);
-            return true;
-        }
+		public Carrito()
+		{
 
 
+		}
 
-        public bool Pagar() //Esta funcion debe ir en el comprador puesto que se necesita acceso 
-        {                   //a su saldo y de acuerdo con el diagrama UML la clase carrito no puede
-            return true;    //ver a la clase comprador
-        }
+		/// <summary>
+		/// Agrega un pedido al carrito.
+		/// </summary>
+		/// <param name="pedido">Pedido que se desea aregar al carrito.</param>
+		/// <returns>Devuelve un booleano que indica si se pudo agregar el pedido.</returns>
+		public bool Agregar(Pedido pedido)
+		{
+			Pedidos.Add(pedido);
+			return true;
+		}
 
-        /// <summary>
-        /// Elimina un pedido del carrito.
-        /// </summary>
-        /// <param name="pedido">Pedido que se desea elimiar del carrito.</param>
-        /// <returns>Devuelve un booleano que indica si se pudo eliminar el pedido.</returns>
-        public bool Eliminar(Pedido pedido)
-        {
-            Pedidos.Remove(pedido);
-            return true;
-        }
-    }
+
+
+		public bool Pagar() //Esta funcion debe ir en el comprador puesto que se necesita acceso 
+		{                   //a su saldo y de acuerdo con el diagrama UML la clase carrito no puede
+			return true;    //ver a la clase comprador
+		}
+
+		/// <summary>
+		/// Elimina un pedido del carrito.
+		/// </summary>
+		/// <param name="pedido">Pedido que se desea elimiar del carrito.</param>
+		/// <returns>Devuelve un booleano que indica si se pudo eliminar el pedido.</returns>
+		public bool Eliminar(Pedido pedido)
+		{
+			Pedidos.Remove(pedido);
+			return true;
+		}
+	}
 }
--- UmlBased/Comprador.cs
+++ UmlBased/Comprador.cs
@@ -5,49 +5,54 @@ using DP = Newtonsoft.Json.JsonPropertyAttribute;
 
 namespace UmlBased
 {
-    [DO(Newtonsoft.Json.MemberSerialization.OptIn)]
-    /// <summary>
-    /// Representa una cuenta de un cliente 
-    /// </summary>
-    public sealed class Comprador : Usuario
-    {
-        /// <summary>
-        /// El tipo de este cliente
-        /// </summary>
-        [DP] public TipoComprador Tipo { get; private set; }
+	[DO(Newtonsoft.Json.MemberSerialization.OptIn)]
+	/// <summary>
+	/// Representa una cuenta de un cliente 
+	/// </summary>
+	public sealed class Comprador : Usuario
+	{
+		/// <summary>
+		/// El tipo de este cliente
+		/// </summary>
+		[DP] public TipoComprador Tipo { get; private set; }
 
-        //Carrito
+		//Carrito
 
-        /// <summary>
-        /// Guarda todos los pedidos que ha realizado este cliente.
-        /// </summary>
-        [DP("Historial")] public List<Pedido> HistorialPedidos { get; } = new();
+		/// <summary>
+		/// Guarda todos los pedidos que ha realizado este cliente.
+		/// </summary>
+		[DP("Historial")] public List<Pedido> HistorialPedidos { get; } = new();
 
-        /// <summary>
-        /// 
-        /// </summary>
-        public void PagoCuotas()
-        {
-            //Logica: Cada inicio de mes el programa principal llama a esta función en cada cliente para que itere su historial de pedidos y descuente una cuota de cada producto.
-        }
+		/// <summary>
+		/// Guarda el carrito de este usuario, no es necesario ponerle el <see cref="DP"/> ya que el carrito no es algo que se deba guardar solo es temporal.
+		/// </summary>
+		public Carrito Carrito = new();
 
-        /// <summary>
-        /// Busca y devuelve todas los pedidos que aun no ha pagado este cliente.
-        /// </summary>
-        public List<Pedido> PagosPendientes()
-        {
-            return null;
-        }
+		/// <summary>
+		/// 
+		/// </summary>
+		public void PagoCuotas()
+		{
+			//Logica: Cada inicio de mes el programa principal llama a esta función en cada cliente para que itere su historial de pedidos y descuente una cuota de cada producto.
+		}
 
-        /// <summary>
-        /// Abona un saldo en un pedido especifico. Esta función se usara por los clientes cuando quiera pagar algo entre sus pedidos.
-        /// </summary>
-        /// <param name="pedido">Factura en la cual se quiere abonar.</param>
-        /// <param name="saldo">Saldo a abonar.</param>
-        /// <returns>Devuelve un valor indicando si ha sido posible o no abonar en esa factura.</returns>
-        public bool Abonar(Pedido pedido, Real saldo)
-        {
-            return true;
-        }
-    }
+		/// <summary>
+		/// Busca y devuelve todas los pedidos que aun no ha pagado este cliente.
+		/// </summary>
+		public List<Pedido> PagosPendientes()
+		{
+			return null;
+		}
+
+		/// <summary>
+		/// Abona un saldo en un pedido especifico. Esta función se usara por los clientes cuando quiera pagar algo entre sus pedidos.
+		/// </summary>
+		/// <param name="pedido">Factura en la cual se quiere abonar.</param>
+		/// <param name="saldo">Saldo a abonar.</param>
+		/// <returns>Devuelve un valor indicando si ha sido posible o no abonar en esa factura.</returns>
+		public bool Abonar(Pedido pedido, Real saldo)
+		{
+			return true;
+		}
+	}
 }
