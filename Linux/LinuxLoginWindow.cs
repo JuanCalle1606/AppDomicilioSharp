182887ecd4c146cb85684816a0a9f45fcf4cc7e6
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-15 16:25:28 -0500

    Terminado el inicio de sesión por ventana

--- Linux/LinuxLoginWindow.cs
+++ Linux/LinuxLoginWindow.cs
@@ -4,12 +4,22 @@ using Gtk;
 using ICommon;
 using ICommon.Bases;
 using KYLib.Interfaces;
+using KYLib.MathFn;
+using UmlBased;
 using UI = Gtk.Builder.ObjectAttribute;
 
 namespace Linux
 {
 	class LinuxLoginWindow : Dialog, ILoginWindow
 	{
+		[UI] Entry LoginCorreo = null;
+
+		[UI] Entry LoginClave = null;
+
+		[UI] Revealer LoginCorreoExp = null;
+
+		[UI] Revealer LoginClaveExp = null;
+
 		public LinuxLoginWindow() : this(new Builder("LinuxLoginWindow.glade")) { }
 
 		private LinuxLoginWindow(Builder builder) : base(builder.GetRawOwnedObject("LinuxLoginWindow"))
@@ -18,6 +28,47 @@ namespace Linux
 			DeleteEvent += Window_DeleteEvent;
 			DefaultResponse = ResponseType.Close;
 			Response += Window_DeleteEvent;
+		}
+
+		private void on_LoginBtn_clicked(object o, EventArgs args)
+		{
+			var correo = LoginCorreo.Text;
+			var clave = LoginClave.Text;
+			Usuario user = DomiciliosApp.Instance.Compradores.Find(C => C.Correo == correo);
+			user ??= DomiciliosApp.Instance.Vendedores.Find(V => V.Correo == correo);
+
+			//si es null es porque no es encuentra ese usuario.
+			if (user == null)
+			{
+				LoginCorreoExp.RevealChild = true;
+			}
+			else
+			{
+				//validamos la clave
+				if (user.Clave == clave)
+				{
+					DomiciliosApp.ClienteActual = user;
+					//llamamos el callback, si existe
+					OnLogin?.Invoke();
+				}
+				else
+				{
+					LoginClaveExp.RevealChild = true;
+				}
+			}
+		}
+
+		private void on_Login_changed(object o, EventArgs args)
+		{
+			var obj = o as Entry;
+			if (obj == null) return;
+
+			var Exp = LoginCorreo == o ? LoginCorreoExp : LoginClaveExp;
+			if (Exp.ChildRevealed)
+				Exp.RevealChild = false;
+
+			//esta linea es solo para lo grafico, una mini animacion para saber cuantos caracteres se pueden poner.
+			obj.ProgressFraction = obj.Text.Length / (float)obj.MaxLength;
 		}
 
 		private void Window_DeleteEvent(object o, SignalArgs args) =>
