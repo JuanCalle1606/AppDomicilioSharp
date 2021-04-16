b4b3780028eaf8403907a704ecec4f30e797143b
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-15 22:07:09 -0500

    Agregado un logo a la aplicación

--- Linux/LinuxFactory.cs
+++ Linux/LinuxFactory.cs
@@ -1,6 +1,8 @@
-using System;
+#pragma warning disable CS0612
+using Gdk;
 using Gtk;
 using ICommon;
+using KYLib.System;
 using UmlBased;
 
 namespace Linux
@@ -15,7 +17,20 @@ namespace Linux
 		public IApp CreateApp()
 		{
 			Application.Init();
+			LoadTheme();
 			return new LinuxApp("org.tori.domiciliosharp", GLib.ApplicationFlags.None);
+		}
+
+		private static void LoadTheme()
+		{
+			var prov = new CssProvider();
+			prov.LoadFromPath(Info.InstallDir + "/Recursos/wind.css");
+			var screen = Gdk.Display.Default.DefaultScreen;
+
+			StyleContext.AddProviderForScreen(screen, prov, StyleProviderPriority.Application);
+
+			IconTheme.AddBuiltinIcon("logo", (int)IconSize.Menu, new Pixbuf(Info.InstallDir + "/Recursos/logo.svg"));
+
 		}
 
 		public ILoginWindow CreateLoginWindow(Usuario DefaultUser, System.Action onUserLogin)
