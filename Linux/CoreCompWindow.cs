d16367b20026e4f53c459fd9ea8b9ff58c4b9cac
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-17 16:43:00 -0500

    Se carga la foto del suuario como miniavatar

--- Linux/CoreCompWindow.cs
+++ Linux/CoreCompWindow.cs
@@ -1,19 +1,36 @@
 using System;
+using System.IO;
 using GLib;
 using Gtk;
 using ICommon.Bases;
+using UmlBased;
+using Pix = Gdk.Pixbuf;
 using UI = Gtk.Builder.ObjectAttribute;
 
 namespace Linux
 {
 	partial class LinuxCompWindow : Window, IWindow
 	{
+		[UI] Image UserAvatar = null;
+
 		public LinuxCompWindow() : this(new Builder("LinuxCompWindow.glade")) { }
 
 		private LinuxCompWindow(Builder builder) : base(builder.GetRawOwnedObject("LinuxCompWindow"))
 		{
 			builder.Autoconnect(this);
 			DeleteEvent += Window_DeleteEvent;
+
+			var foto = DomiciliosApp.ClienteActual.Foto;
+			if (!string.IsNullOrWhiteSpace(foto) && File.Exists(foto))
+			{
+				if (Pix.GetFileInfo(foto, out _, out _) != null)
+				{
+					using (var temp = new Pix(foto))
+					{
+						UserAvatar.Pixbuf = temp.ScaleSimple(20, 20, Gdk.InterpType.Tiles);
+					}
+				}
+			}
 		}
 
 		/// <summary>
@@ -21,5 +38,8 @@ namespace Linux
 		/// </summary>
 		private void Window_DeleteEvent(object o, EventArgs args) =>
 			Gtk.Application.Quit();
+
+		/// <inheritdoc/>
+		void IWindow.Show() => ShowAll();
 	}
 }
