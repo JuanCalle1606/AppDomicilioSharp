0571d9504c071b5f18ee67c7e52cb4f9cecefb25
JuanCalle1606 <jpmilojp@gmail.com>
2021-04-23 22:58:13 -0500

    Terminado ProductoDialogo

--- Linux/Widgets/ProductoDialogo.cs
+++ Linux/Widgets/ProductoDialogo.cs
@@ -1,6 +1,8 @@
 using System;
 using Gtk;
 using KYLib.Extensions;
+using KYLib.MathFn;
+using KYLib.Utils;
 using Linux.Extensions;
 using UmlBased;
 using UI = Gtk.Builder.ObjectAttribute;
@@ -13,16 +15,17 @@ namespace Linux.Widgets
 		[UI] Label Titulo = null;
 		[UI] Label Precio = null;
 		[UI] Label Domicilio = null;
-		//[UI] SpinButton Calificar = null;
-		//[UI] Button CalificarBtn = null;
+		[UI] SpinButton Calificar = null;
+		[UI] Button CalificarBtn = null;
+		[UI] Label CalificacionDialogo = null;
 		[UI] Image Foto = null;
 		[UI] Label DescripcionDialogo = null;
 		[UI] SpinButton CuotasDialogo = null;
-		//[UI] Label NombreVendedor = null;
-		//[UI] Label CalificacionVendedor = null;
-		//[UI] Button CarritoBtnDialogo = null;
-		//[UI] Button CerrarBtn = null;
+		[UI] Label NombreVendedor = null;
+		[UI] Label CalificacionVendedor = null;
 		[UI] Box AdmiteCuotas = null;
+
+		bool calificable = true;
 
 		bool ShowDialog = false;
 
@@ -33,6 +36,19 @@ namespace Linux.Widgets
 			Dialogo.Show();
 		}
 
+		void on_CalificarBtn_clicked(object o, EventArgs args)
+		{
+			if (!calificable) return;
+			calificable = false;
+			Small puntaje = (Small)Calificar.ValueAsInt;
+			Producto.Calificar(puntaje);
+			CalificarBtn.Sensitive = false;
+			Calificar.Sensitive = false;
+			var cal = "Calificación: {0} ".Format(Producto.Calificacion);
+			CalificacionDialogo.Text = cal;
+			Calificacion.Text = cal;
+		}
+
 		void ConfigurarDialogo()
 		{
 			Dialogo.DeleteEvent += OnDialogDeleted;
@@ -40,10 +56,12 @@ namespace Linux.Widgets
 			Titulo.Text = Producto.Name;
 			DescripcionDialogo.Text = DescripcionDialogo.Text.Format(Producto.Descripcion);
 			Precio.Text = Precio.Text.Format(Producto.Precio);
+			CalificacionDialogo.Text = CalificacionDialogo.Text.Format(Producto.Calificacion);
 			Domicilio.Text = Domicilio.Text.Format(Producto.ValorDomicilio);
 			AdmiteCuotas.Visible = Producto.PermiteCuotas;
-			//var vendedor = Producto.ObtenerVendedor();
-			//NombreVendedor.Text = vendedor.Name;
+			var vendedor = Producto.ObtenerVendedor();
+			NombreVendedor.Text = vendedor?.Name;
+			CalificacionVendedor.Text = CalificacionVendedor.Text.Format(vendedor?.Calificacion);
 			Foto.Load(Producto.Foto, 250, 250);
 
 		}
