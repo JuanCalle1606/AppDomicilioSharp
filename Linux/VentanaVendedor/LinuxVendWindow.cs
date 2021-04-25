659e809c4a68b49599f61e37afcbae078b67c59d
Juancarlosarbelaez <juan.arbelaezm@upb.edu.co>
2021-04-25 15:25:15 -0500

    Avance en el codigo asociado a la ventana del vendedor

--- Linux/VentanaVendedor/LinuxVendWindow.cs
+++ Linux/VentanaVendedor/LinuxVendWindow.cs
@@ -1,5 +1,9 @@
 using System;
 using Gtk;
+using Linux.Widgets;
+using KYLib.Extensions;
+using KYLib.MathFn;
+using Linux.Extensions;
 using UmlBased;
 using UI = Gtk.Builder.ObjectAttribute;
 
@@ -7,21 +11,70 @@ namespace Linux
 {
 	class LinuxVendWindow : Window
 	{
-
+		/// <summary>
+		/// Vendedor que esta siendo mostrado aqui.
+		/// </summary>
 		public Vendedor Vendedor { get; private set; }
+
+		/// <summary>
+		/// Imagen del vendedor.
+		/// </summary>
+		[UI] Image Imagen = null;
+
+		/// <summary>
+		/// Label que muestra el nombre del vendedor.
+		/// </summary>
+		[UI] Label Nombre = null;
+
+		/// <summary>
+		/// Label que muestra el telefono del vendedor.
+		/// </summary>
+		[UI] Label Telefono = null;
+
+		/// <summary>
+		/// Label que muestra el correo del vendedor.
+		/// </summary>
+		[UI] Label Correo = null;
+
+		/// <summary>
+		/// Label que muestra la direccion del vendedor.
+		/// </summary>
+		[UI] Label Direccion = null;
+
+		/// <summary>
+		/// Label que muestra el horario de atencion del vendedor.
+		/// </summary>
+		[UI] Label HroAtencion = null;
+
+		/// <summary>
+		/// Label que muestra la calificacion del vendedor.
+		/// </summary>
+		[UI] Label Calificacion = null;
+
+		[UI] ListBox ListaProductos = null;
 		public LinuxVendWindow(Vendedor vendedor) : this(new Builder("LinuxVendWindow.glade"), vendedor) { }
 
 
 		private LinuxVendWindow(Builder builder, Vendedor vendedor) : base(builder.GetRawOwnedObject("LinuxVendWindow"))
 		{
 			builder.Autoconnect(this);
+			Vendedor = vendedor;
+			Imagen.Load(Vendedor.Foto, 250, 250);
+			Nombre.Text = Nombre.Text.Format(vendedor.Name);
+			Telefono.Text = Telefono.Text.Format(vendedor.Telefono);
+			Direccion.Text = Direccion.Text.Format(vendedor.Direccion);
+			HroAtencion.Text = HroAtencion.Text.Format(vendedor.AtencionCliente);
+			Calificacion.Text = Calificacion.Text.Format(vendedor.Calificacion);
 			ConfigurarMenu();
 			builder.Dispose();
 		}
 
 		private void ConfigurarMenu()
 		{
-			var User = (Vendedor)DomiciliosApp.ClienteActual
+			foreach (var item in Vendedor.Menu)
+			{
+				ListaProductos.Add(new ProductoWidget(item));
+			}
 		}
 	}
 }
