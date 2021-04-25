74add5ab26c560ff24ae39baa5df05b0a776f1ce
Juancarlosarbelaez <juan.arbelaezm@upb.edu.co>
2021-04-25 14:38:31 -0500

    Terminada la ventana del Vendedor

--- Linux/VentanaVendedor/LinuxVendWindow.cs
+++ Linux/VentanaVendedor/LinuxVendWindow.cs
@@ -1,17 +1,27 @@
 using System;
 using Gtk;
+using UmlBased;
 using UI = Gtk.Builder.ObjectAttribute;
 
 namespace Linux
 {
 	class LinuxVendWindow : Window
 	{
-		public LinuxVendWindow() : this(new Builder("LinuxVendWindow.glade")) { }
 
-		private LinuxVendWindow(Builder builder) : base(builder.GetRawOwnedObject("LinuxVendWindow"))
+		public Vendedor Vendedor { get; private set; }
+		public LinuxVendWindow(Vendedor vendedor) : this(new Builder("LinuxVendWindow.glade"), vendedor) { }
+
+
+		private LinuxVendWindow(Builder builder, Vendedor vendedor) : base(builder.GetRawOwnedObject("LinuxVendWindow"))
 		{
 			builder.Autoconnect(this);
+			ConfigurarMenu();
 			builder.Dispose();
+		}
+
+		private void ConfigurarMenu()
+		{
+			var User = (Vendedor)DomiciliosApp.ClienteActual
 		}
 	}
 }
