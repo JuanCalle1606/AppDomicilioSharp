cdb0ec307f8cc885b03cb6e63cfece942be6a6b9
JuanCalle1606 <jpmilojp@gmail.com>
2021-05-03 18:38:41 -0500

    AL comprador se le muestran productos al azar

--- Linux/Ventana Comprador/ShowCompWindow.cs
+++ Linux/Ventana Comprador/ShowCompWindow.cs
@@ -29,25 +29,31 @@ namespace Linux
 			var foto = DomiciliosApp.ClienteActual.Foto;
 			UserAvatar.Load(foto, 20, 20);
 
-			//esto es solo para pruebas, agregamos productos random
-			for (int i = 0; i < 10; i++)
-			{
-				var prod = new Producto()
-				{
-					Name = Rand.GetInt(1023, 1216).ToString(),
-					Descripcion = Rand.GetInt(0, 9, 10, 20).ToString(' '),
-					Precio = Rand.GetInt(1000, 100000),
-					ValorDomicilio = Rand.GetInt(0, 15000),
-					FechaCreacion = DateTime.Now,
-					Foto = "https://media.discordapp.net/attachments/783188322087993346/834245259964973096/2021-04-20_22_52_39-Window.png"
-				};
-				var c = Rand.GetInt(0, 11, 1, 11);
-				foreach (var item in c)
-				{
-					prod.Calificar((Small)item);
-				}
+			ShowProductos();
+		}
 
-				ListaProductos.Add(new ProductoWidget(prod));
+		void ShowProductos()
+		{
+			if (DomiciliosApp.Instance.Productos.Count == 0) return;
+
+			var i = 0;
+			foreach (var item in DomiciliosApp.Instance.Productos.Shuffle())
+			{
+				ListaProductos.Add(new ProductoWidget(item));
+				if (i++ >= 10) break;
+			}
+		}
+
+		void ClearProductos()
+		{
+			var current = ListaProductos.Children;
+			foreach (ListBoxRow item in current)
+			{
+				using (item)
+				{
+					ListaProductos.Remove(item);
+					item.Child.Dispose();
+				}
 			}
 		}
 	}
